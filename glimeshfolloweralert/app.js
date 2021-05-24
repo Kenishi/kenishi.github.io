6e1b89c30aa59c5607005daa8fefb2b18ad52eee
Jeremy May <kenishi86@gmail.com>
2021-05-24 05:04:43 -0400

    Trying to fix oauth redirect loop

--- glimeshfolloweralert/app.js
+++ glimeshfolloweralert/app.js
@@ -1,22 +1,18 @@
-function oauthClient() {
-	const config = {
-		providerID: "glimesh_follower_alert",
-		client_id: "8d7b7d340da3182af3e356ca46938883474ce08f1172d6569df3fb44f56d5dd3",
-		redirect_uri: "https://kenishi.github.io/glimeshfolloweralert",
-		authorization: "https://glimesh.tv/oauth/authorize",
-		token: "https://glimesh.tv/api/oauth/token",
-		response_type: "code",
-		client_secret: atob(r()),
-		scopes: { request: ["public"] }
-	};
-
-	return new jso.JSO(config);
-}
+const config = {
+	providerID: "glimesh_follower_alert",
+	client_id: "8d7b7d340da3182af3e356ca46938883474ce08f1172d6569df3fb44f56d5dd3",
+	redirect_uri: "https://kenishi.github.io/glimeshfolloweralert",
+	authorization: "https://glimesh.tv/oauth/authorize",
+	token: "https://glimesh.tv/api/oauth/token",
+	response_type: "code",
+	client_secret: atob(r()),
+	scopes: { request: ["public"] }
+};
+const oauthClient = jso.JSO(config);
+// Handle potential redirect arrivals w/ credentials
+oauthClient.callback();
 
 async function onPageLoad() {
-	// Handle potential redirect arrivals w/ credentials
-	oauthClient().callback();
-
 	// Get tokens, including triggering an auth handshake if needed
 	const token = await oauthClient().getToken();
 
