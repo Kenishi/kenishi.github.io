9b3141f8dfdd5f889252890c77a5b83cc657276b
Jeremy May <kenishi86@gmail.com>
2021-05-24 05:34:51 -0400

    Debugging

--- glimeshfolloweralert/app.js
+++ glimeshfolloweralert/app.js
@@ -19,8 +19,8 @@ console.log("callback handled");
 async function onPageLoad() {
 	console.log("getting token");
 	// Get tokens, including triggering an auth handshake if needed
-	// const token = await oauthClient.getToken();
-	// console.log("Token received: " + token);
+	const token = await oauthClient.getToken();
+	console.log("Token received: " + token);
 
 	// Subscribe for follower information
 	// subscribeForFollowerAlerts(token);
