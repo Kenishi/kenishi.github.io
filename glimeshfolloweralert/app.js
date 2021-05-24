22f9a5372311028bd199d2281829a37bf2c71466
Jeremy May <kenishi86@gmail.com>
2021-05-24 05:13:34 -0400

    Adding debugger code

--- glimeshfolloweralert/app.js
+++ glimeshfolloweralert/app.js
@@ -1,3 +1,4 @@
+debugger;
 const config = {
 	providerID: "glimesh_follower_alert",
 	client_id: "8d7b7d340da3182af3e356ca46938883474ce08f1172d6569df3fb44f56d5dd3",
@@ -9,12 +10,16 @@ const config = {
 	scopes: { request: ["public"] }
 };
 const oauthClient = new jso.JSO(config);
+console.log("client made, checking callback");
 // Handle potential redirect arrivals w/ credentials
 oauthClient.callback();
+console.log("callback handled");
 
 async function onPageLoad() {
+	console.log("getting token");
 	// Get tokens, including triggering an auth handshake if needed
 	const token = await oauthClient.getToken();
+	console.log("Token received: " + token);
 
 	// Subscribe for follower information
 	subscribeForFollowerAlerts(token);
